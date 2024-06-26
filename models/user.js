4914889f43993d5ba16c043c3b4c2bd6ddab2df1
RKY <xraj2023@gmail.com>
2024-06-26 17:16:40 +0530

    Creating the Users table

--- models/user.js
+++ models/user.js
@@ -1,26 +1,52 @@
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
-
-const User = sequelize.define('users', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  phoneNo: {
-    type: Sequelize.INTEGER,
-    allowNull: false
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+// id name email phoneNo
+class User {
+  constructor(name, email, phoneNo, id) {
+    this.name = name;
+    this.email = email;
+    this.phoneNo = phoneNo;
+    this._id = id ? new mongodb.ObjectId(id) : null ;
   }
-});
+
+  save () {
+    const db = getDb();
+    let dbOp;
+    if(this._id){
+      // Update the product
+      return dbOp = db.collection('users').updateOne({_id: this._id}, {$set: this});
+    } else {
+      return dbOp = db.collection('users').insertOne(this);
+    }
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('users').find({
+
+    }).toArray()
+    .then(users => {
+      // console.log(users);
+      return users;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  static findUserById(prodId) {
+    const db = getDb();
+    return db.collection('users').findOne({
+        _id: new mongodb.ObjectId(prodId)
+    })
+    .then(users => {
+      console.log(users);
+      return users;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+}
 
 module.exports = User;
